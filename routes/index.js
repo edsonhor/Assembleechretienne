d1acbfdd74350fc2d999596db41c6e2f4d15a07f
edsonhor <edson.philippe@ufl.edu>
2015-12-12 22:13:38 +0000

    fixing extra character that git added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
-<<<<<<< HEAD
 var passport= require('passport');
 var Strategy= require('passport-local').Strategy;
-=======
->>>>>>> e4a270a9f3b44722380f9e0558ee7345ec74d4e3
 var router = express.Router();
 
 /* GET home page. */
@@ -11,7 +8,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-<<<<<<< HEAD
 
 router.post ('/onboarding', function( req, res ){
         if(!req.body) return res.sendStatus(400)
@@ -22,6 +18,4 @@ router.get ('/test', function( req, res ){
            res.send('We GOT THE PAYLOAD');
         });
 
-=======
->>>>>>> e4a270a9f3b44722380f9e0558ee7345ec74d4e3
 module.exports = router;
