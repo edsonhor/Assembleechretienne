16e0a0b7ef24a53882180dbb645bd5fa81de2b87
Edson H Philippe <edson.philippe@gmail.com>
2016-06-22 02:04:51 -0400

    Cleaned out extra file

--- public/javascripts/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var onboarding_component_1 = require('./onboarding.component');
-platform_browser_dynamic_1.bootstrap(onboarding_component_1.OnboardingComponent);
-//# sourceMappingURL=main.js.map
