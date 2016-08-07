18b3fc321479f2baa79c193487e07b5189ad9ba0
Edson H Philippe <edson.philippe@gmail.com>
2016-08-06 23:34:59 -0400

    More changes for angular 2

--- easywebpack.js
+++ easywebpack.js
@@ -5,6 +5,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
+        polyfills: './public/javascripts/polyfills.ts',
         vendor: "./public/javascripts/vendor",
         bundle: "./public/javascripts/main"},
   output: {
